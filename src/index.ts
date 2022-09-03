53ecebf134de43265074ce851fa02fa4ebc6a20f
kit <tingiao.ki@ttpsc.com>
2022-09-03 16:21:35 +0800

    Finish decorator.

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser  from 'body-parser';
+import cookieSession from 'cookie-session';
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
+import './controllers/RootController';
+
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['vbnm,fghj'] }));
+app.use(AppRouter.getInstance());
+
+app.listen(3000, (): void => {
+    console.log('listening on port 3000'); 
+});
